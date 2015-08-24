4650a39bae8fcb2a523b7758001fcb7dfbb796e6
Peter Bons <msn@2000miles.nl>
2015-08-24 14:23:37 +0200

    Reuse HttpClient

--- SemanticLogging.EventHub/EventHubHttpSink.cs
+++ SemanticLogging.EventHub/EventHubHttpSink.cs
@@ -7,12 +7,13 @@ using SemanticLogging.EventHub.Utility;
 
 namespace SemanticLogging.EventHub
 {
-    public class EventHubHttpSink : IObserver<EventEntry>
+    public class EventHubHttpSink : IObserver<EventEntry>, IDisposable
     {
         private readonly string eventHubNamespace;
         private readonly string eventHubName;
         private readonly string publisherId;
         private readonly string sasToken;
+        private readonly HttpClient httpClient = new HttpClient();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EventHubHttpSink" /> class.
@@ -32,32 +33,29 @@ namespace SemanticLogging.EventHub
             this.eventHubName = eventHubName;
             this.publisherId = publisherId;
             this.sasToken = sasToken;
+
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", sasToken);
         }
 
         public async void OnNext(EventEntry value)
         {
-            using (var httpClient = new HttpClient())
+            var payload = JsonConvert.SerializeObject(value);
+
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+            content.Headers.Add("ContentType", "application/atom+xml;type=entry;charset=utf-8");
+            var url = string.Format("https://{0}.servicebus.windows.net/{1}/publishers/{2}/Messages", eventHubNamespace, eventHubName, publisherId);
+
+            try
             {
-                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", sasToken);
+                var response = await httpClient.PostAsync(url, content);
+                if (!response.IsSuccessStatusCode)
+                    SemanticLoggingEventSource.Log.CustomSinkUnhandledFault(string.Format("The request failed with statuscode {0}: {1}", (int)response.StatusCode, response.ReasonPhrase));
 
-                var payload = JsonConvert.SerializeObject(value);
-
-                var content = new StringContent(payload, Encoding.UTF8, "application/json");
-                content.Headers.Add("ContentType", "application/atom+xml;type=entry;charset=utf-8");
-                var url = string.Format("https://{0}.servicebus.windows.net/{1}/publishers/{2}/Messages", eventHubNamespace, eventHubName, publisherId);
-
-                try
-                {
-                    var response = await httpClient.PostAsync(url, content);
-                    if(!response.IsSuccessStatusCode)
-                        SemanticLoggingEventSource.Log.CustomSinkUnhandledFault(string.Format("The request failed with statuscode {0}: {1}", (int)response.StatusCode, response.ReasonPhrase));
-
-                }
-                catch (Exception ex)
-                {
-                    SemanticLoggingEventSource.Log.CustomSinkUnhandledFault(ex.ToString());
-                    throw;
-                }
+            }
+            catch (Exception ex)
+            {
+                SemanticLoggingEventSource.Log.CustomSinkUnhandledFault(ex.ToString());
+                throw;
             }
         }
 
@@ -69,6 +67,19 @@ namespace SemanticLogging.EventHub
         public void OnCompleted()
         {
 
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                httpClient.Dispose();
+            }
         }
     }
 }
